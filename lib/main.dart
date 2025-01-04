47af4f847525dcdb6e5f609486a518a6f74547be
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-04 20:45:21 +0100

    Wrap the HomePage with BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:bloc_tutorial/cubit/counter_cubit.dart';
 import 'package:bloc_tutorial/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +12,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: HomePage(),
+      ),
+    );
   }
 }
