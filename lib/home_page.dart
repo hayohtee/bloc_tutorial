70c8589b87f5b0b71a89c3102bd6e370ee2e464a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-04 20:23:12 +0100

    Update HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,47 @@
+import 'package:bloc_tutorial/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final counter = CounterCubit();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text("You have pushed the button this many times:"),
+            ),
+            BlocBuilder<CounterCubit, int>(
+              bloc: counter,
+              builder: (context, state) => Text(
+                state.toString(),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.increment(),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
