93dadf148de530b15b20f59193a20873539659ca
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-09 12:24:49 +0100

    Register CounterIncremented event

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterBloc extends Bloc<CounterIncremented, int> {
-  CounterBloc() : super(0);
+  CounterBloc() : super(0) {
+    on<CounterIncremented>((event, emit) {
+      emit(state + 1);
+    });
+  }
 }
 
 class CounterIncremented {}
