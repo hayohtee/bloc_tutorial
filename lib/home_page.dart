df89da3a78a8ad82aab55682a9df9df9b493d7ce
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-09 12:52:19 +0100

    Remove import

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,8 +2,6 @@ import 'package:bloc_tutorial/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'bloc/counter_event.dart';
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
